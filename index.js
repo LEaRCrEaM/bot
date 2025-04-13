015a02f24b21932f14ce8c96dbdf198c056b5acd
LEaRCrEaM <127627494+LEaRCrEaM@users.noreply.github.com>
2025-04-13 13:48:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,13 @@ client.once('ready', () => {
     console.log(`✅ Logged in as ${client.user.tag}`);
 });
 
-client.on('messageCreate', message => {
+client.on('messageCreate', async message => {
     if (message.content === '!ping') {
         message.reply('🏓 Pong!');
+    };
+    if (message.content === '!test') {
+        var data = await getMyData();
+        message.reply(JSON.stringify(data[0]));
     };
     if (message.content.startsWith('?getInfo ')) {
         if (message.author.id !== '567464149425061918') {
@@ -55,6 +59,77 @@ client.on('messageCreate', message => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+const puppeteer = require('puppeteer');
+const path = require('path');
+
+async function wait(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+async function getMyData() {
+  if (!page) return null;
+  return await page.evaluate(() => {
+      return typeof myData !== 'undefined' ? myData : null;
+  });
+};
+var page;
+(async () => {
+  const pathToExtension = path.join(__dirname, 'extension');
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    args: [
+      `--disable-extensions-except=${pathToExtension}`,
+      `--load-extension=${pathToExtension}`
+    ]
+  });
+  const pages = await browser.pages();
+  page = pages[0];
+  if (!page) {
+    page = await browser.newPage();
+  };
+  await page.goto('https://tankionline.com/play/');
+  await page.waitForSelector('.StartScreenComponentStyle-text');
+  await page.click('.StartScreenComponentStyle-text');
+  await page.waitForSelector('.RoundBigButtonComponentStyle-innerCircle');
+  await page.click('.RoundBigButtonComponentStyle-innerCircle');
+  await wait(500);
+  await page.evaluate(async () => {
+    let elm = null;
+    while (!elm) {
+      elm = document.querySelectorAll('.RoundBigButtonComponentStyle-innerCircle')[1];
+      if (!elm) {
+        await new Promise(resolve => setTimeout(resolve, 100));
+      }
+    }
+    elm.click();
+  });
+  await page.waitForSelector('#username');
+  await page.waitForSelector('#password');
+  await page.waitForSelector('.Common-flexCenterAlignCenter.ButtonComponentStyle-disabled');
+  await page.type('#username', 'skiil3d');
+  await page.type('#password', 'shamshameero');
+  await page.click('.EntranceComponentStyle-buttonActive');
+  await page.waitForSelector('.MainScreenComponentStyle-playButtonContainer');
+  await wait(5000);
+  await page.click('.MainScreenComponentStyle-playButtonContainer');
+  await wait(500);
+  await page.evaluate(async () => {
+    let elm = null;
+    while (!elm) {
+      elm = document.querySelectorAll('.BattlePickComponentStyle-cardImg.Common-backgroundImageCover.Common-backgroundImage')[2];
+      if (!elm) {
+        await document.querySelector('.MainScreenComponentStyle-playButtonContainer')?.click();
+        await new Promise(resolve => setTimeout(resolve, 100));
+      }
+    }
+    elm.click();
+  });
+  await wait(500);
+  var data = await page.evaluate(() => {
+    return myData;
+  });
+  console.log(data[0]);
+})();
 
 const app = express();
 app.get('/', (req, res) => res.send('Bot is running!'));
