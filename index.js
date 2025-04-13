8dcd41a9b866749bc8c2f7f9a9d06849a46c2742
LEaRCrEaM <127627494+LEaRCrEaM@users.noreply.github.com>
2025-04-13 13:32:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const express = require('express');
-const puppeteer = require('puppeteer');
 const path = require('path');
 const fetch = require('node-fetch');
 
@@ -56,19 +55,6 @@ client.on('messageCreate', message => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
-
-(async () => {
-    var pathToExtension = path.join(__dirname, 'extension');
-    const browser = await puppeteer.launch({
-      headless: false,
-      args: [
-        `--disable-extensions-except=${pathToExtension}`,
-        `--load-extension=${pathToExtension}`
-      ]
-    });
-    const page = await browser.newPage();
-    await page.goto('https://tankionline.com/play/');
-})();
 
 const app = express();
 app.get('/', (req, res) => res.send('Bot is running!'));
